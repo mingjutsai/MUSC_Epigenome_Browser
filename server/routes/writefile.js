0246fefdd136a494b26bfe84887b99be666764f1
Ming-Ju <milutsai.bi98g@g2.nctu.edu.tw>
2021-04-18 11:05:52 -0400

    add path to igv

--- server/routes/writefile.js
+++ server/routes/writefile.js
@@ -1,24 +1,43 @@
 const router = require('express').Router();
-const fs = require('fs')
-const hic_resolution = process.env.HIC_RESOLUTION;
-
-require('dotenv').config();
-
+const fs = require('fs');
+//require('dotenv').config();
+//const igv_path = process.env.IGV_PATH;
+const igvDir = __dirname + '/../../public/igv/'
+var igvInput;
 router.route('/').post((req, res) => {
     if(req.body.type == "snp"){
         const rsid = req.body.post;
         const content = req.body.range;
-        fs.writeFile('../public/igv/' + rsid + '.bed', content, err => {
-            if(err) {
-                console.log(err)
-                return
-            }
-        })
+        igvInput = igvDir + rsid + '.bed'
+        //const filePath = __dirname + '/../../public/igv/'
+        if(fs.existsSync(igvDir)){
+            console.log('Directory exists!')
+            //fs.writeFile('../public/igv/' + rsid + '.bed', content, err => {
+            fs.writeFile(igvInput, content, err => {
+                    if(err) {
+                        console.log(err)
+                        return err
+                    }
+                })
+        }else{
+            console.log('Directory not found.')
+            return 'Directory not found.';
+        }
+        //console.log(filePath)
+        // fs.writeFile('../public/igv/' + rsid + '.bed', content, err => {
+        // //fs.writeFile(igv_path + rsid + '.bed', content, err => {
+        //     if(err) {
+        //         console.log(err)
+        //         return
+        //     }
+        // })
     }else if(req.body.type == "interaction"){
         const rsid = req.body.rsid;
         const content = req.body.content;
         const cell = req.body.cell;
-        fs.writeFile('../public/igv/' + rsid + '_' + cell + '.bedpe.txt', content, err => {
+        igvInput = igvDir + rsid + '_' + cell + '.bedpe.txt';
+        //fs.writeFile('../public/igv/' + rsid + '_' + cell + '.bedpe.txt', content, err => {
+        fs.writeFile(igvInput, content, err => {
             if(err) {
                 console.log(err)
                 return
@@ -28,7 +47,9 @@ router.route('/').post((req, res) => {
         const gene = req.body.gene;
         const content = req.body.content;
         const cell = req.body.cell;
-        fs.writeFile('../public/igv/' + gene + '_' + cell + '.bedpe.txt', content, err => {
+        igvInput = igvDir + gene + '_' + cell + '.bedpe.txt';
+        //fs.writeFile('../public/igv/' + gene + '_' + cell + '.bedpe.txt', content, err => {
+        fs.writeFile(igvInput, content, err => {
             if(err) {
                 console.log(err)
                 return
@@ -36,8 +57,20 @@ router.route('/').post((req, res) => {
         })
     }
     res.send(
-        `Successfully generate file: ${req.body.type}`,
+        `Successfully generate file: ${igvInput}`,
     )
+    // if(fs.existsSync(igvInput)){
+    //     res.send(
+    //         `Successfully generate file: ${igvInput}`,
+    //     )
+    // }else{
+    //     res.send(
+    //         `Failed generate file: ${igvInput}`,
+    //     )
+    // }
+    // res.send(
+    //     `Successfully generate file: ${req.body.type}`,
+    // )
     
 });
 
